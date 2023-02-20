bd0251ae85eac7b747678942664b3bb5276d4dd7
Feterson1 <Feterson@bk.ru>
2023-02-20 21:25:24 +0600

    added_categories_to_admin_panel

--- resources/views/admin/layouts/main.blade.php
+++ resources/views/admin/layouts/main.blade.php
@@ -8,20 +8,20 @@
   <!-- Google Font: Source Sans Pro -->
   <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
   <!-- Font Awesome -->
-  <link rel="stylesheet" href="plugins/fontawesome-free/css/all.min.css">
+  <link rel="stylesheet" href='{{asset("plugins/fontawesome-free/css/all.min.css")}}'>
   <!-- Ionicons -->
-  <link rel="stylesheet" href="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css">
+  <link rel="stylesheet" href='{{asset("https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css")}}'>
   <!-- Tempusdominus Bootstrap 4 -->
  
   <!-- iCheck -->
   
   
   <!-- Theme style -->
-  <link rel="stylesheet" href="dist/css/adminlte.min.css">
+  <link rel="stylesheet" href='{{asset("dist/css/adminlte.min.css")}}'>
   <!-- overlayScrollbars -->
-  <link rel="stylesheet" href="plugins/overlayScrollbars/css/OverlayScrollbars.min.css">
+  <link rel="stylesheet" href='{{asset("plugins/overlayScrollbars/css/OverlayScrollbars.min.css")}}'>
   <!-- Daterange picker -->
-  <link rel="stylesheet" href="plugins/daterangepicker/daterangepicker.css">
+  <link rel="stylesheet" href='{{asset("plugins/daterangepicker/daterangepicker.css")}}'>
   <!-- summernote -->
   
 </head>
@@ -30,7 +30,7 @@
 
   <!-- Preloader -->
   <div class="preloader flex-column justify-content-center align-items-center">
-    <img class="animation__shake" src="dist/img/AdminLTELogo.png" alt="AdminLTELogo" height="60" width="60">
+    <img class="animation__shake" src='{{asset("dist/img/AdminLTELogo.png")}}' alt="AdminLTELogo" height="60" width="60">
   </div>
 
   <!-- Navbar -->
@@ -48,17 +48,7 @@
   </nav>
   <!-- /.navbar -->
 
-  <!-- Main Sidebar Container -->
-  <aside class="main-sidebar sidebar-dark-primary elevation-4">
-    <!-- Brand Logo -->
-   
-
-    <!-- Sidebar -->
-    <div class="sidebar">
-     
-    </div>
-    <!-- /.sidebar -->
-  </aside>
+  @include('admin.includes.sidebar')
 
   @yield('content')
 
@@ -75,9 +65,9 @@
 <!-- ./wrapper -->
 
 <!-- jQuery -->
-<script src="plugins/jquery/jquery.min.js"></script>
+<script src='{{asset("plugins/jquery/jquery.min.js")}}'></script>
 <!-- jQuery UI 1.11.4 -->
-<script src="plugins/jquery-ui/jquery-ui.min.js"></script>
+<script src='{{asset("plugins/jquery-ui/jquery-ui.min.js")}}'></script>
 <!-- Resolve conflict in jQuery UI tooltip with Bootstrap tooltip -->
 <script>
   $.widget.bridge('uibutton', $.ui.button)
@@ -93,16 +83,16 @@
 <!-- jQuery Knob Chart -->
 
 <!-- daterangepicker -->
-<script src="plugins/moment/moment.min.js"></script>
-<script src="plugins/daterangepicker/daterangepicker.js"></script>
+<script src='{{asset("plugins/moment/moment.min.js")}}'></script>
+<script src='{{asset("plugins/daterangepicker/daterangepicker.js")}}'></script>
 <!-- Tempusdominus Bootstrap 4 -->
 
 <!-- Summernote -->
 
 <!-- overlayScrollbars -->
-<script src="plugins/overlayScrollbars/js/jquery.overlayScrollbars.min.js"></script>
+<script src='{{asset("plugins/overlayScrollbars/js/jquery.overlayScrollbars.min.js")}}'></script>
 <!-- AdminLTE App -->
-<script src="dist/js/adminlte.js"></script>
+<script src='{{asset("dist/js/adminlte.js")}}'></script>
 <!-- AdminLTE for demo purposes -->
 
 <!-- AdminLTE dashboard demo (This is only for demo purposes) -->
--- routes/admin.php
+++ routes/admin.php
@@ -1,10 +1,15 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\Admin\Main\IndexController;
+use App\Http\Controllers\Admin\Main\AdminController;
+use App\Http\Controllers\Admin\Category\CategoryController;
 
 
 
 Route::prefix('admin')->group(function(){
-    Route::get('/',IndexController::class);
+    Route::get('/',AdminController::class);
+    Route::prefix('categories')->group(function(){
+        Route::get('/',[CategoryController::class,'index']);
+    });
+
 });
