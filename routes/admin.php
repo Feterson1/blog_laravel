6e88975c1a51c631f77c25adf78b36d91ee4646d
Feterson1 <Feterson@bk.ru>
2023-02-24 23:02:04 +0600

    added_CRUD_for_tags

--- routes/admin.php
+++ routes/admin.php
@@ -3,6 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Admin\Main\AdminController;
 use App\Http\Controllers\Admin\Category\CategoryController;
+use App\Http\Controllers\Admin\Tag\TagController;
 
 
 
@@ -18,4 +19,15 @@ Route::prefix('admin')->group(function(){
         Route::delete('/{category}',[CategoryController::class,'delete'])->name('admin.category.delete');
     });
 
+    Route::prefix('tags')->group(function(){
+        Route::get('/',[TagController::class,'index'])->name('admin.tag.index');
+        Route::get('/create',[TagController::class,'create'])->name('admin.tag.create');
+        Route::post('/store',[TagController::class,'store'])->name('admin.tag.store');
+        Route::get('/{tag}',[TagController::class,'show'])->name('admin.tag.show');
+        Route::get('/{tag}/edit',[TagController::class,'edit'])->name('admin.tag.edit');
+        Route::patch('/{tag}',[TagController::class,'update'])->name('admin.tag.update');
+        Route::delete('/{tag}',[TagController::class,'delete'])->name('admin.tag.delete');
+    });
+
 });
+
