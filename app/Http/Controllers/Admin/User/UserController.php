ad348d158dc80c225b5ea5e90d59b044fe7c2ccf
Feterson1 <Feterson@bk.ru>
2023-02-27 20:08:47 +0600

    role_users

--- app/Http/Controllers/Admin/User/UserController.php
+++ app/Http/Controllers/Admin/User/UserController.php
@@ -19,12 +19,13 @@ class UserController extends Controller
     }
 
     public function create(){
-
-        return view('admin.users.create');
+        $roles = User::getRoles();
+        return view('admin.users.create',compact('roles'));
     }
     public function store(StoreRequest $request){
 
         $data = $request->validated();
+        
         $data['password'] = Hash::make($data['password']);
         User::firstOrCreate(['email' => $data['email']],$data);
         return redirect()->route('admin.user.index');
@@ -35,8 +36,8 @@ class UserController extends Controller
         
     }
     public function edit(User $user){
-        
-        return view('admin.users.edit',compact('user'));
+        $roles = User::getRoles();
+        return view('admin.users.edit',compact('user','roles'));
 
     }
     public function update(UpdateRequest $request,User $user){
